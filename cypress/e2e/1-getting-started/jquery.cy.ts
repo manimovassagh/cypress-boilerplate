a8824c42ff773e1b8c18589fc6ed5d742abfed25
Mani Movassagh <mani.mgh@gmail.com>
2023-03-13 23:53:48 +0100

    match

--- cypress/e2e/1-getting-started/jquery.cy.ts
+++ cypress/e2e/1-getting-started/jquery.cy.ts
@@ -9,7 +9,26 @@ it('test jquery', () => {
         expect($button.hasClass('btn btn_primary btn_small btn_inventory')).to.be.exist
         expect($button).to.have.class('btn btn_primary btn_small btn_inventory')
             .not.to.be.disabled
-
         cy.log(JSON.stringify($button.css.name))
+
+        //.and('match', /serif/)
     })
+
+    cy.get('*[id^="add-to-cart"]')
+        .then(($button) => {
+
+            expect($button.last()).to.contain('Add')
+            expect($button.first()).to.contain('Add')
+            expect($button.first())
+
+        })
+
+    cy.get('*[id^="add-to-cart"]').should(($buttons) => {
+        expect($buttons.eq(0)).not.to.be.disabled
+
+
+    })
+    cy.get('*[id^="add-to-cart"]').invoke('attr', 'id').should('match', /add/)
+
+
 });
