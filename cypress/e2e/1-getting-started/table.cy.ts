355281765dbf731c790fc41f62aa4bef89c6e608
Mani Movassagh <mani.mgh@gmail.com>
2023-03-22 02:57:26 +0100

    handle error

--- cypress/e2e/1-getting-started/table.cy.ts
+++ cypress/e2e/1-getting-started/table.cy.ts
@@ -28,3 +28,31 @@ it('confirms the a row', { baseUrl: 'https://tablepress.org/demo/' }, function (
     //         }),
     //     )
 })
+
+it.only('confirms the a row', { baseUrl: 'https://tablepress.org/demo/' }, function () {
+    cy.visit('/')
+    cy.get('table tbody tr')
+        .find('td')
+        .map('innerText')
+        .print()
+        .partial(Cypress._.zipObject, props)
+        .should(spok({
+            firstName: 'Gloria',    
+            Zip: spok.test(/^\d{5}$/),
+            Birthday: Cypress._.isString,
+        }))
+    // cy.contains('table tbody tr', 'Dave')
+    //     .find('td')
+    //     .map('innerText')
+    //     .print()
+    //     .partial(Cypress._.zipObject, props)
+    //     .print()
+    //     .update('age', Number)
+    //     .should(
+    //         spok({
+    //             name: 'Dave',
+    //             age: spok.range(10, 19),
+    //             date: spok.test(/^\d\d\d\d-\d\d-\d\d$/),
+    //         }),
+    //     )
+})
