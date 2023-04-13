4d74c19db88a086a42dd038a8a9f066ada5484a0
Mani Movassagh <mani.mgh@gmail.com>
2023-04-13 23:26:56 +0200

    check

--- cypress/e2e/1-getting-started/request.cy.ts
+++ cypress/e2e/1-getting-started/request.cy.ts
@@ -3,14 +3,14 @@ it('check request', () => {
     cy.request('GET', 'https://jsonplaceholder.typicode.com/todos/1')
         .its('body')
         .should('have.a.property', 'completed')
-        
 
+    //just a chekc
     cy.request('GET', 'https://jsonplaceholder.typicode.com/todos')
         .its("body").each($el => {
             expect($el).to.have.property('completed').and.not.to.be.undefined.and.not.to.be.null
             assert.equal([1, 2, 3].indexOf(4), -1);
         })
     cy.log('<h1>check some markdown</h1>')
-    
+
 
 })
