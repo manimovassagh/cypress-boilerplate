a25414892e28ce9d86d26f2cfea0c5aa01391564
Mani Movassagh <mani.mgh@gmail.com>
2023-04-21 00:36:04 +0200

    snapshot

--- cypress/e2e/recipies/doppleOption.cy.ts
+++ cypress/e2e/recipies/doppleOption.cy.ts
@@ -1,6 +1,8 @@
 it('dopple option', () => {
     //selector
     cy.get('#states + .select2')
-    .contains('.select2-selection__choice', 'Connecticut')
-    .find('.select2-selection__choice__remove').click()
+        .contains('.select2-selection__choice', 'Connecticut')
+        .find('.select2-selection__choice__remove').click()
+
+    cy.get('a').screenshot()
 });
