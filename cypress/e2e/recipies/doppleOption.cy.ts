30a39e3a502bf21e593621401430b8e02cde08ae
Mani Movassagh <mani.mgh@gmail.com>
2023-04-20 23:34:57 +0200

    selector reciepe

--- cypress/e2e/recipies/doppleOption.cy.ts
+++ cypress/e2e/recipies/doppleOption.cy.ts
@@ -1,4 +1,5 @@
 it('dopple option', () => {
+    //selector
     cy.get('#states + .select2')
     .contains('.select2-selection__choice', 'Connecticut')
     .find('.select2-selection__choice__remove').click()
