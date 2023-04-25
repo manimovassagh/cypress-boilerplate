a702bf1833d2e6b0e113186d79c3e07b15471fe7
Mani Movassagh <mani.mgh@gmail.com>
2023-04-25 02:24:57 +0200

    check

--- cypress/e2e/recipies/doppleOption.cy.ts
+++ cypress/e2e/recipies/doppleOption.cy.ts
@@ -1,8 +1,11 @@
 it('dopple option', () => {
-    //selector test
-    cy.get('#states + .select2')
-        .contains('.select2-selection__choice', 'Connecticut')
-        .find('.select2-selection__choice__remove').click()
+
+    //go to base url with /about page
+cy.visit('https://testautomationpractice.blogspot.com/')
+
+    //click on dopple option
+    cy.get('#HTML9 > div.widget-content > select').select('Dopple')
     cy.log('checkpoint')
-    cy.get('a').screenshot()
-});
+
+})
+
