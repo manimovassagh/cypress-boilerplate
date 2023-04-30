5930914d8a59b01b0d1bc570a0eb55b64b40e44d
Mani Movassagh <mani.mgh@gmail.com>
2023-04-30 13:47:27 +0200

    test

--- cypress/e2e/1-getting-started/myTest.cy.ts
+++ cypress/e2e/1-getting-started/myTest.cy.ts
@@ -30,7 +30,7 @@ describe("Ultimate QA Automation Page", () => {
   });
 
   it("should have the correct header", () => {
-    //check the header of page 
+    //check the header of page test
     cy.visit("https://jsonplaceholder.typicode.com");
     // cy.get('.et_pb_text_inner h1').should('have.text', 'Automation Practice')
     cy.request("GET", "https://jsonplaceholder.typicode.com/todos").then(
