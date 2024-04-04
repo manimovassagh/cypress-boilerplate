edd7b5ff8015d34256f90634aabaf4b3f6a6e10a
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2024-04-04 21:49:13 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,39 +1,10 @@
-import { defineConfig } from 'cypress'
-const { verifyDownloadTasks } = require('cy-verify-downloads');
-const mysql = require('mysql');
+const { defineConfig } = require("cypress");
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mmmmmm",
-  database: 'cypress'
-
-});
-
-export default defineConfig({
-
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5500/',
-    //chromeWebSecurity: false,
-    experimentalStudio: true,
-    env: {
-      api_url: 'working !!!!!'
-    },
-
-
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      on('task', verifyDownloadTasks);
-      on('task', {
-        async resetDatabase() {
-          // Do Database reset
-          console.log('Seed database is done');
-          console.log('Seed database is done');
-
-          return null;
-        }
-      });
-
+      // implement node event listeners here
     },
+    
   },
-})
+});
