4b4b53754ebcb29e20e1ee04a42d15dc049850bd
Mani Movassagh <mani.mgh@gmail.com>
2024-04-23 17:54:35 +0200

    test

--- cypress/e2e/1-getting-started/unhappy.cy.ts
+++ cypress/e2e/1-getting-started/unhappy.cy.ts
@@ -1,24 +1,21 @@
-
 import { names } from "../../fixtures/names";
-
 
 describe("unhappy", () => {
   it("chcck reporter", () => {
-    
-    const name = names.nameOne
+    const name = names.nameOne;
     expect(name).to.be.equal(name);
   });
   // The test checks if the value of the variable 'name' is not equal to 'john'
 
   it("chcck reporter with different name", () => {
-    const name = names.nameThree
+    const name = names.nameThree;
     expect(name).to.not.be.equal(names.nameOne);
   });
   // The test checks if the value of the variable 'name' is not empty
 
   it("chcck reporter with non-empty name", () => {
     const name = names.nameOne;
-    expect(name).to.not.be.empty;	
+    expect(name).to.not.be.empty;
   });
   // Testing string equality with case-insensitivity
 
